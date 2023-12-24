57c1d6bf99927a8cb5490cbf36ab2560f3b91e62
Ananda Fitra <anandadf@student.ub.ac.id>
2023-12-25 00:15:40 +0700

    feat: add blog controllers

---
+++ controllers/blogController.js
@@ -0,0 +1,71 @@
+const prisma = require("../model/connection");
+const bindBodyOrError = require("../utils/binds");
+const { sendError, sendSuccess } = require("../utils/sendResponse");
+
+async function getBlogs(req, res) {
+  const reqBody = bindBodyOrError(req, res, "page");
+  if (reqBody == null) return;
+
+  const blogs = await prisma.blogs.findMany({
+    select: {
+      slug: true,
+      date_posted: true,
+      title: true,
+      description: true,
+    },
+    orderBy: {
+      date_posted: "desc"
+    },
+    skip: reqBody.page * 10,
+    take: 10
+  });
+  sendSuccess(res, {
+    "blogs": blogs
+  });
+}
+
+async function getBlogDetail(req, res) {
+  const blogSlug = req.params['blogSlug'];
+
+  const blog = await prisma.blogs.findUnique({
+    where: {
+      slug: blogSlug
+    },
+  });
+  if (!blog) {
+    sendError(res, 404, "Blog not found.");
+    return;
+  } 
+  sendSuccess(res, {
+    "id": blog.id,
+    "title": blog.title,
+    "slug": blog.slug,
+    "date_posted": blog.date_posted,
+    "description": blog.description
+  });
+}
+
+async function postBlog(req, res) {
+  const reqBody = bindBodyOrError(req, res, "title", "slug", "description");
+  if (reqBody == null) return;
+
+  const newBlog = await prisma.blogs.create({
+    data: {
+      date_posted: (new Date()).toISOString(),
+      title: reqBody.title,
+      slug: reqBody.slug,
+      description: reqBody.description
+    }
+  });
+  if (!newBlog.id) {
+    sendError(res, 503, "Error creating blog in server, please try again later.");
+    return;
+  }
+  sendSuccess(res, {
+    "message": "New blog has been created."
+  });
+}
+
+module.exports.getBlogs = getBlogs;
+module.exports.getBlogDetail = getBlogDetail;
+module.exports.postBlog = postBlog;
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const log = require('../middleware/logger');
 const { sendSuccess } = require('../utils/sendResponse');
 const { getDescription } = require('../controllers/homeController');
+const { getBlogDetail, getBlogs, postBlog } = require('../controllers/blogController');
 
 const app = express();
 
 app.use(log);
+app.use(express.json());
 
 // Ping function
 app.get('/ping', (_, res) => {
@@ -16,5 +18,10 @@ app.get('/ping', (_, res) => {
 
 // Home page 
 app.get('/profile/description', getDescription);
+
+// Blogs page
+app.get('/blog/', getBlogs);
+app.get('/blog/:blogSlug', getBlogDetail);
+app.post('/blog', postBlog);
 
 module.exports = app;
