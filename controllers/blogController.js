2587db68f7dad92523a63113781f6a548a5001c2
Ananda Fitra <anandadf@student.ub.ac.id>
2024-01-09 14:07:55 +0700

    refactor(blog): error handle

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -20,6 +20,7 @@ async function getBlogs(req, res) {
     skip: reqBody.page * 10,
     take: 10
   });
+
   sendSuccess(res, {
     "blogs": blogs
   });
@@ -28,22 +29,23 @@ async function getBlogs(req, res) {
 async function getBlogDetail(req, res) {
   const blogSlug = req.params['blogSlug'];
 
-  const blog = await prisma.blogs.findUnique({
-    where: {
-      slug: blogSlug
-    },
-  });
-  if (!blog) {
+  try {
+    const blog = await prisma.blogs.findUnique({
+      where: {
+        slug: blogSlug
+      },
+    });
+    sendSuccess(res, {
+      "id": blog.id,
+      "title": blog.title,
+      "slug": blog.slug,
+      "date_posted": blog.date_posted,
+      "description": blog.description
+    });
+  } catch (e) {
     sendError(res, 404, "Blog not found.");
     return;
   } 
-  sendSuccess(res, {
-    "id": blog.id,
-    "title": blog.title,
-    "slug": blog.slug,
-    "date_posted": blog.date_posted,
-    "description": blog.description
-  });
 }
 
 async function postBlog(req, res) {
@@ -55,55 +57,54 @@ async function postBlog(req, res) {
     sendError(res, 400, "'title' cannot be more than 100 characters.");
     return;
   }
-
-  const existBlogs = await prisma.blogs.findUnique({
-    where: {
-      slug: reqBody.slug
+  
+  try {
+    const newBlog = await prisma.blogs.create({
+      data: {
+        date_posted: (new Date()).toISOString(),
+        title: reqBody.title,
+        slug: reqBody.slug,
+        short_description: reqBody.short_description,
+        description: reqBody.description
+      }
+    });
+    if (!newBlog.id) {
+      sendError(res, 503, "Error creating blog in server, please try again later.");
+      return;
     }
-  });
-  if (existBlogs) {
+    sendSuccess(res, {
+      "message": "New blog has been created."
+    });
+  } catch (e) {
     sendError(res, 409, "Blog already posted, check your slug");
     return;
   }
 
-  const newBlog = await prisma.blogs.create({
-    data: {
-      date_posted: (new Date()).toISOString(),
-      title: reqBody.title,
-      slug: reqBody.slug,
-      short_description: reqBody.short_description,
-      description: reqBody.description
-    }
-  });
-  if (!newBlog.id) {
-    sendError(res, 503, "Error creating blog in server, please try again later.");
-    return;
-  }
-  sendSuccess(res, {
-    "message": "New blog has been created."
-  });
 }
 
 async function editBlog(req, res) {
   const blogSlug = req.params['blogSlug'];
   const reqBody = bindBodyOrError(req, res, "title", "short_description", "description");
-  const updateBlog = await prisma.blogs.update({
-    where: {
-      slug: blogSlug
-    },
-    data: {
-      title: reqBody['title'],
-      short_description: reqBody['short_description'],
-      description: reqBody['description']
-    }
-  });
-  if (!updateBlog) {
+  if (reqBody == null) return;
+
+  try {
+    await prisma.blogs.update({
+      where: {
+        slug: blogSlug
+      },
+      data: {
+        title: reqBody['title'],
+        short_description: reqBody['short_description'],
+        description: reqBody['description']
+      }
+    });
+    sendSuccess(res, {
+      "message": `Blog ${blogSlug} has been edited successfully.`
+    });
+  } catch (e) {
     sendError(res, 404, "Blog not found.");
     return;
   }
-  sendSuccess(res, {
-    "message": `Blog ${blogSlug} has been edited successfully.`
-  });
 }
 
 async function deleteBlog(req, res) {
